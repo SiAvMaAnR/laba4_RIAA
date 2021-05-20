2895243db2cf36a302ca76a2ed14599379622773
SiAvMaAnR <samarkin200220022002@gmail.com>
2021-05-21 00:38:18 +0300

    .

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("laba5_RIAA")]
+[assembly: AssemblyTitle("laba4_RIAA")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("laba5_RIAA")]
+[assembly: AssemblyProduct("laba4_RIAA")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
