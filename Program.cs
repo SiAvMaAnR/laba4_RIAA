e708a448c956c24dea3538a356c47d96286280cc
SiAvMaAnR <samarkin200220022002@gmail.com>
2021-06-10 11:31:08 +0300

    /

--- Program.cs
+++ Program.cs
@@ -14,9 +14,9 @@ namespace laba5_RIAA
             for (var h = (Math.Abs(a) + Math.Abs(b)) / 2.0; ; h /= 2.0)
             {
                 I2 = 0.0;
-                for (var x = a+h; x <= b; x += h)
+                for (var x = a; x < b; x += h)
                 {
-                    I2 += f(x - h) * h;
+                    I2 += f(x) * h;
                 }
 
                 if (Math.Abs(I2 - I1) <= eps)
@@ -33,7 +33,7 @@ namespace laba5_RIAA
             for (var h = (Math.Abs(a) + Math.Abs(b)) / 2.0; ; h /= 2.0)
             {
                 I2 = 0.0;
-                for (var x = a; x < b; x += h)
+                for (var x = a + h; x <= b; x += h)
                 {
                     I2 += f(x) * h;
                 }
@@ -52,9 +52,9 @@ namespace laba5_RIAA
             for (var h = (Math.Abs(a) + Math.Abs(b)) / 2.0; ; h /= 2.0)
             {
                 I2 = 0.0;
-                for (var x = a; x <= b; x += h)
+                for (var x = a; x < b; x += h)
                 {
-                    I2 += f(x - h/2) * h;
+                    I2 += f(x + h / 2) * h;
                 }
 
                 if (Math.Abs(I2 - I1) <= eps)
@@ -71,7 +71,7 @@ namespace laba5_RIAA
             for (var h = (Math.Abs(a) + Math.Abs(b)) / 2.0; ; h /= 2.0)
             {
                 I2 = 0.0;
-                for (var x = a; x <= b; x += h)
+                for (var x = a+h; x <= b; x += h)
                 {
                     I2 += (f(x - h) + 4 * f(x - h / 2) + f(x)) * h / 6;
                 }
@@ -90,9 +90,9 @@ namespace laba5_RIAA
             for (var h = (Math.Abs(a) + Math.Abs(b)) / 2.0; ; h /= 2.0)
             {
                 I2 = 0.0;
-                for (var x = a; x <= b; x += h)
+                for (var x = a; x < b; x += h)
                 {
-                    I2 += (f(x - h) + f(x)) / 2 * h;
+                    I2 += (f(x) + f(x + h)) / 2 * h;
                 }
 
                 if (Math.Abs(I2 - I1) <= eps)
@@ -114,11 +114,11 @@ namespace laba5_RIAA
         static void Run()
         {
             //локальная функция
-            double f(double x) => Math.Sin(2 * Math.PI * x);
+            double f(double x) => Math.Cos(2 * Math.PI * x);
 
             double a = 0;
-            double b = 1.0/4;
-            double eps = 0.001;
+            double b = 1.0 / 4;
+            double eps = 0.00001;
 
             List<Task<double>> tasks = new List<Task<double>>();
             tasks.Add(new Task<double>(() => LeftRectangle(f, a, b, eps)));
@@ -127,15 +127,15 @@ namespace laba5_RIAA
             tasks.Add(new Task<double>(() => Trapezoid(f, a, b, eps)));
             tasks.Add(new Task<double>(() => Simpson(f, a, b, eps)));
 
-            tasks.ForEach(x=>x.Start());
+            tasks.ForEach(x => x.Start());
 
             Task.WaitAll(tasks.ToArray());
 
-            Console.WriteLine("Формула левых прямоугольников: {0}", tasks[0].Result);
-            Console.WriteLine("Формула правых прямоугольников: {0}", tasks[1].Result);
-            Console.WriteLine("Формула средних прямоугольников: {0}", tasks[2].Result);
-            Console.WriteLine("Формула трапеции: {0}", tasks[3].Result);
-            Console.WriteLine("Формула Симпсона: {0}", tasks[4].Result);
+            Console.WriteLine($"Формула левых прямоугольников: {tasks[0].Result}");
+            Console.WriteLine($"Формула правых прямоугольников: {tasks[1].Result}");
+            Console.WriteLine($"Формула средних прямоугольников: {tasks[2].Result}");
+            Console.WriteLine($"Формула трапеции: {tasks[3].Result}");
+            Console.WriteLine($"Формула Симпсона: {tasks[4].Result}");
         }
     }
 }
